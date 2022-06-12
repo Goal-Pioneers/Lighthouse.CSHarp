fa111b3c34a989f0c86790ad251316a166ed3bcc
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-06-12 18:17:30 +0200

    working on system

--- Code/Lighthouse/Program.cs
+++ Code/Lighthouse/Program.cs
@@ -1,31 +1,74 @@
-﻿using System;
+﻿/**
+  * 
+  */
+using System;
+
 
 namespace Lighthouse
 {
     class Program
     {
-        static void Main( string[] args )
+        /**
+         * 
+         */
+        static void Main( string[] arguments )
         {
-            Program program = new Program();
-
-            program.initialise();
-            program.execute();
-            program.garbageCollection();
+            Singleton.initialise();
+            Singleton.execute();
+            Singleton.garbageCollection();
         }
 
+        // Variables
+        private static Program singleton = null;
+
+
+        /**
+         * 
+         */
         public void initialise()
         {
-
+            Console.WriteLine( "initialise" );
         }
 
+
+        /**
+         * 
+         */
         public void execute()
         {
-
+            Console.WriteLine( "execution" );
         }
 
+
+        /**
+         * 
+         */
         public void garbageCollection()
         {
+            Console.WriteLine( "cleaning system" );
+        }
 
+
+        // Accessors
+        /**
+         * 
+         */
+        public static Program Singleton
+        {
+            get
+            {
+                if ( singleton == null )
+                {
+                    Singleton = new Program();
+                }
+
+                return singleton;
+            }
+            set
+            {
+                singleton = value;
+            }
         }
     }
 }
+
